b1879a111bc39ae3acaf4368d546eed2f52c0038
noor <noorhafowbare@gmail.com>
2025-03-14 14:28:31 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// strings in js
+// let firstName = "noor";
+// let secondName = "isse";
+// console.log(firstName);
+
+// string concatination
+
+let firstName = "         noor           ";
+let secondName = "isse";
+// let fullName = firstName + " " + secondName;
+
+// other ways of string concatination
+// let fullName = firstName.concat(secondName);
+// console.log(fullName);
+
+// 2. append
+// firstName += "bare";
+// console.log(firstName);
+
+// length
+console.log(firstName.length);
+
+//cases
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+
+// slice in string
+console.log(firstName.slice(1, 4));
+
+// split and join
+console.log(firstName.split("").join(" "));
+
+// includes
+console.log(firstName.includes("n"));
+//trim
+
+console.log(firstName.trim());
